1217fb730561ca04267a5911e89e342f29834107
nicolas_facon <nicolas_facon@ens.univ-artois.fr>
2022-11-25 08:38:00 +0100

    from decorator to composite to save

--- src/ImageSaveComposite.py
+++ src/ImageSaveComposite.py
@@ -1,20 +1,21 @@
 from PIL import Image
 import os
+from ImageSplitterDecorator import ImageSplitterDecorator
 
 
-class ImageSaveDecorator:
+class ImageSaveComposite:
     """
 
-    ImageSaveDecorator class, used to save images correctly at the good point.
+    ImageSaveComposite class, used to save images correctly at the good point.
 
     To do its job, the class needs an image, a path and a name. To get the
     filename the constructor make a concatenation between path and name.
 
     """
-    def __init__(self, decore: Image, path: str, name: str) -> None:
+    def __init__(self, path: str, name: str, type: str) -> None:
         """
 
-        ImageSaveDecorator constructor, needs an img, a path, a name.
+        ImageSaveComposite constructor, needs an img, a path, a name.
 
         In this constructor, the filename is got by a
         concatenation between the path and the name values.
@@ -23,10 +24,10 @@ class ImageSaveDecorator:
         :rtype: None
 
         """
-        self.decore = decore
-        self.path = path
+        self.images = []
+        self.path = path + '/'
         self.name = name
-        self.filename = path + '/' + name
+        self.type = type
 
     def save(self) -> None:
         """
@@ -42,4 +43,21 @@ class ImageSaveDecorator:
         """
         if not os.path.exists(self.path):
             os.mkdir(self.path)
-        self.decore.save(self.filename)
+        for i in range(len(self.images)):
+            name = self.path + self.name + str(i) + '.' + self.type
+            self.images[i].save(name)
+
+    def append(self, image: Image) -> None:
+        self.images.append(image)
+
+    def remove(self, image: Image) -> None:
+        try:
+            self.images.remove(image)
+        except ValueError:
+            pass
+
+    @staticmethod
+    def from_images_to_composite(images: list[Image], path: str, name: str, type: str) -> object:
+        composite = ImageSaveComposite(path, name, type)
+        composite.images = images
+        return composite
